9b465853071d9e2203484106610d78e6a99b6d0f
CakeDallas <vh3429196@gmail.com>
2025-11-22 09:38:44 -0300

    EstruturaAtulizada

--- src/pages/Dashboard/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import "./Dashboard.css";
-
-export default function Dashboard() {
-  return (
-    <div className="dashboard">
-      <h2 className="dashboard-title">Dashboard</h2>
-    </div>
-  );
-}
